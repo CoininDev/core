0bc1b6dda5a275f6723b84065cde686ee0e9e598
gaybe <gabr.batz@gmail.com>
2023-01-14 13:14:18 -0300

    urls

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('the_screen_of_adm/', admin.site.urls),
     path('', include('noob.urls'))
 ]
